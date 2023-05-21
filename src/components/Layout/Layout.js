c26b5560ee97586e3640bb05cea92c0a7213ae17
Vitor Avelino Sena <vitoravelinosena1@gmail.com>
2023-05-20 22:50:14 -0300

    feat: Development of the initial visual part and the server list

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,39 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function Layout() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Server Monitor</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Server List</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <script
+        src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js"
+        crossorigin
+      ></script>
+
+      <script
+        src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+        crossorigin
+      ></script>
+
+      <script
+        src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+        crossorigin
+      ></script>
+
+      <script>var Alert = ReactBootstrap.Alert;</script>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/components/ServerReader/ServerReader.js
@@ -0,0 +1,10 @@
+function ServerReader() {
+  return (
+    <div className="ServerReader">
+     HELLO
+    </div>
+  );
+}
+export default ServerReader;
+
+
